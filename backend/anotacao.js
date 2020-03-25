091aef417798b5b324125c0c5f63c23e8b3edfd1
vinive <vinicius_ve_@hotmail.com>
2020-03-25 03:47:11 -0300

    backend da aplicação

---
+++ backend/anotacao.js
@@ -0,0 +1,35 @@
+/**
+ * Métodos HTTP
+ * 
+ * GET: Buscar/Listar uma informação do back-end
+ * POST: Criar uma informação no back-end
+ * PUT: Alterar uma informação no back-end
+ * DELETE: Deletar uma informação no back-end
+ */
+
+ /**
+  * Tipos de parâmetros: 
+  * 
+  * Query Params: Parãmetros nomeados enviados na rota após "?" (filtros, paginação)
+  * Route Params: Parâmetros utilizados para identificar recursos
+  * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
+  */
+
+  /**
+   * Retrono Undefined = Não conseguiu entender o corpo 
+   */
+
+   /**
+    * Criar uma tabela vazia: 
+    * npx knex migrate:make create_nome da tabela
+    * 
+    * Criar uma tabela com os dados: 
+    * npx knex migrate:make create_nome da tabela
+    * 
+    * Desfazer a ultima vez que o comando foi executado:
+    * npx knex migrate:rollback
+    * 
+    * Mostrar todos os comandos:
+    * npx knex
+    *
+    */
